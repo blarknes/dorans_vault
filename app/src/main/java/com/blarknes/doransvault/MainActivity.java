ba7ebbbfad52cb4768cd7c9732f4dd0e7db12334
blarknes <villasgb@gmail.com>
2021-06-08 22:54:41 -0300

    test v2

--- app/src/main/java/com/blarknes/doransvault/MainActivity.java
+++ app/src/main/java/com/blarknes/doransvault/MainActivity.java
@@ -1,8 +1,28 @@
 package com.blarknes.doransvault;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
