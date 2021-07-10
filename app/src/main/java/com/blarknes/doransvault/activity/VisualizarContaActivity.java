b069706830c4abc48eec312b9abc09e3c4e7b551
blarknes <villasgb@gmail.com>
2021-07-10 05:33:10 -0300

    added decay identifier

--- app/src/main/java/com/blarknes/doransvault/activity/VisualizarContaActivity.java
+++ app/src/main/java/com/blarknes/doransvault/activity/VisualizarContaActivity.java
@@ -4,31 +4,34 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.text.InputType;
 import android.text.method.PasswordTransformationMethod;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.blarknes.doransvault.DAO.ContaDAO;
 import com.blarknes.doransvault.R;
 import com.blarknes.doransvault.model.Conta;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-
-import java.io.BufferedInputStream;
 import java.io.InputStream;
+import java.util.Calendar;
 import java.util.List;
 
 public class VisualizarContaActivity extends AppCompatActivity {
@@ -37,11 +40,14 @@ public class VisualizarContaActivity extends AppCompatActivity {
     ProgressDialog progressDialog;
     String url, urlChampion,  region,
            eloHolder, eloFlexHolder,
-           nickHolder, subtitleHolder;
+           nickHolder, subtitleHolder,
+           decayHolder;
     ImageView imgElo, imgEloFlex, imgIcon, imgBanner;
-    TextView nick, login, senha, elo, eloFlex, subtitle;
+    TextView nick, login, senha, elo, eloFlex, subtitle, decay;
     Bitmap bitmapElo, bitmapEloFlex, bitmapIcon, bitmapBanner;
     ImageButton showPassword;
+    int[] games = new int[10];
+    public static String result, urlRanked;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -104,6 +110,7 @@ public class VisualizarContaActivity extends AppCompatActivity {
         subtitle = findViewById(R.id.subtitle);
         elo = findViewById(R.id.elo);
         eloFlex = findViewById(R.id.eloFlex);
+        decay = findViewById(R.id.decayLabel);
         imgElo = findViewById(R.id.imageElo);
         imgEloFlex = findViewById(R.id.imageEloFlex);
         imgIcon = findViewById(R.id.imageIcon);
@@ -221,6 +228,21 @@ public class VisualizarContaActivity extends AppCompatActivity {
                 }
 
 
+                if (eloHolder.contains("Mestre") || eloHolder.contains("Desafiante")) {
+                    try {
+                        Element requestCodeDoc = doc.select("li.current.allChampions > a").last();
+                        String requestCode = requestCodeDoc.absUrl("data-additional-url-base");
+                        requestCode = requestCode.substring(requestCode.indexOf(region.toLowerCase()));
+                        requestCode = requestCode.substring(requestCode.indexOf("/") + 1);
+                        requestCode = requestCode.substring(0, requestCode.indexOf("/"));
+                        String requestCodeAux = region.toLowerCase() + "/" + requestCode;
+                        urlRanked = String.format("https://www.leagueofgraphs.com/partial/summoner/recent/%s/0/queueId-420", requestCodeAux);
+
+                    } catch (Exception e) {
+                        decayHolder = "Você está decaindo";
+                    }
+                }
+
 
             } catch (Exception e) {
                 e.printStackTrace();
@@ -233,6 +255,9 @@ public class VisualizarContaActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
+
+            grayScale();
+
             nick.setText(nickHolder);
 
             if (bitmapIcon != null)
@@ -247,7 +272,81 @@ public class VisualizarContaActivity extends AppCompatActivity {
             subtitle.setText(subtitleHolder);
             elo.setText(eloHolder);
             eloFlex.setText(eloFlexHolder);
+
+            decayCalculator(urlRanked);
+
             progressDialog.dismiss();
         }
     }
+
+    public void grayScale(){
+        ColorMatrix matrix = new ColorMatrix();
+        matrix.setSaturation(.55f);
+        imgBanner.setColorFilter(new ColorMatrixColorFilter(matrix));
+    }
+
+    public void decayCalculator(String urlRanked) {
+        RequestQueue queue = Volley.newRequestQueue(VisualizarContaActivity.this);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, urlRanked,
+            response -> {
+                Boolean hasMoreGames = true;
+
+                while (hasMoreGames) {
+                    String responseAux = "";
+                    if (response.contains("gameDate")) {
+                        responseAux = response.substring(response.indexOf("gameDate")+8);
+                        responseAux = responseAux.substring(responseAux.indexOf("\">")+2);
+                        responseAux = responseAux.substring(0, responseAux.indexOf("</div>")).trim();
+
+                        int timeNumber;
+                        if (responseAux.contains("A day ago"))
+                            games[1]++;
+                        else if (responseAux.contains("min") || responseAux.contains("hour")) {
+                            Calendar rightNow = Calendar.getInstance();
+                            timeNumber = responseAux.contains("An") ? 1 : Integer.parseInt(responseAux.substring(0, responseAux.indexOf(" ")));
+                            if (rightNow.get(Calendar.HOUR_OF_DAY) - timeNumber < 0)
+                                games[1]++;
+                            else
+                                games[0]++;
+                        } else {
+                            timeNumber = Integer.parseInt(responseAux.substring(0, responseAux.indexOf(" ")));
+                            if (timeNumber < 10)
+                                games[timeNumber]++;
+                        }
+
+                        response = response.substring(response.indexOf("gameDate")+8);
+                    } else
+                        hasMoreGames = false;
+                }
+
+                int countDays = 10;
+                for (int i = 9; i >= 0;  i--) {
+                    countDays += games[i]-1;
+                    if (countDays > 10)
+                        countDays = 10;
+                }
+
+                if (countDays == 0)
+                    result = "Você decairá hoje";
+                else if (countDays == 1)
+                    result = "Você decairá em 1 dia";
+                else
+                    result = "Você decairá em "+countDays+" dias";
+
+                Log.v("ranked", result);
+
+                decayHolder = result;
+                decay.setText(decayHolder);
+            },
+            new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    result = "erro";
+                }
+            });
+        queue.add(stringRequest);
+
+    }
+
 }
